5bda56bd9ae01da7549767cb0e809c10a1a1b0c8
unknown <suzieboo@gmail.com>
2020-03-25 22:33:55 +1300

    changing vpc in asg4

--- asg.tf
+++ asg.tf
@@ -90,3 +90,37 @@ module "demo_asg" {
     },
   ]
 }
+######
+# ELB
+######
+module "elb" {
+  source = "terraform-aws-modules/elb/aws"
+
+  name = "elb-demo"
+
+  subnets         = data.aws_subnet_ids.all.ids
+  security_groups = [data.aws_security_group.default.id]
+  internal        = false
+
+  listener = [
+    {
+      instance_port     = "3000"
+      instance_protocol = "HTTP"
+      lb_port           = "3000"
+      lb_protocol       = "HTTP"
+    },
+  ]
+
+  health_check = {
+    target              = "HTTP:3000/"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
