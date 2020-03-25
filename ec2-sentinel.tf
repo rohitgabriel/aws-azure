4fdbf13f6ed24b73211d2f85fb3322fb59a1e14f
unknown <suzieboo@gmail.com>
2020-03-25 19:05:38 +1300

    sentinel test on ec2 instances

--- ec2-sentinel.tf
+++ ec2-sentinel.tf
@@ -1,17 +1,24 @@
-resource "aws_instance" "pipeline" {
-  ami           = var.AMI_ID
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
-  # the VPC subnet
-  subnet_id = aws_subnet.main-public-1.id
-
-  # the security group
-  vpc_security_group_ids = [aws_security_group.pipeline-instance.id]
-
-  # the public SSH key
-  key_name = aws_key_pair.pipeline-key.key_name
-
   tags = {
-    Name = "pipeline-centos"
+    Name = "HelloWorld"
   }
 }
