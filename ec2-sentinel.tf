e359d0cb8cc680a2bda6119cc13ebcc042b7f6e1
unknown <suzieboo@gmail.com>
2020-03-25 19:04:08 +1300

    sentinel test on ec2 instance

---
+++ ec2-sentinel.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "pipeline" {
+  ami           = var.AMI_ID
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.pipeline-instance.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.pipeline-key.key_name
+
+  tags = {
+    Name = "pipeline-centos"
+  }
+}
