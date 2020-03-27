e455496b9600ca84ddedee895ff201c7a648f394
unknown <suzieboo@gmail.com>
2020-03-27 15:21:30 +1300

    checkout git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,52 @@
 pipeline {
     agent any
+    environment {
+        AWS_CRED=credentials('AWS-Account-Credentials')
+        AWS_ACCESS_KEY_ID="${AWS_CRED_USR}"
+        AWS_SECRET_ACCESS_KEY="${AWS_CRED_PSW}"
+        AWS_DEFAULT_REGION="ap-southeast-2"
+    }
     stages {
+        stage("Checkout repo") {
+            steps {
+                checkout([
+                    $class: 'GITSCM'
+                    branches: [[name: 'refs/head/master']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [[
+                        $class: 'SubmoduleOption',
+                        disableSubmodules: false,
+                        parentCredentials: false,
+                        recursiveSubmodules: false,
+                        reference: '',
+                        trackingSubmodules: false
+                    ]],
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[url: 'https://github.com/rohitgabriel/aws-azure.git']]
+                ])
+            }
+        }
+        stage("test") {
+            steps {
+                sh '''
+                pwd
+                '''
+            }
+        }
+        stage("test") {
+            steps {
+                sh '''
+                pwd
+                '''
+            }
+        }
+        stage("test") {
+            steps {
+                sh '''
+                pwd
+                '''
+            }
+        }
         stage("test") {
             steps {
                 sh '''
